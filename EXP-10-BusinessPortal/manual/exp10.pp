7d9934768144c05ca4bee8f32838681b270f5d4e
ciprianpavel <ciprian.pavel@gmail.com>
2015-11-24 11:16:26 +0200

    Apache + php working

--- EXP-10-BusinessPortal/manual/exp10.pp
+++ EXP-10-BusinessPortal/manual/exp10.pp
@@ -1,32 +1,32 @@
-#install apache
-#include apache
+  class { 'apache':                # use the "apache" module
+    default_vhost => false,        # don't use the default vhost
+    default_mods => false,         # don't load default mods
+    mpm_module => 'prefork',        # use the "prefork" mpm_module
+  }
+   include apache::mod::php        # include mod php
+   apache::vhost { 'example.com':  # create a vhost called "example.com"
+    port    => '80',               # use port 80
+    docroot => '/var/www/html',     # set the docroot to the /var/www/html
+  }
+#class { 'mysql::server': 
+#	root_password => 'strongpassword',
+#	remove_default_accounts => true ,
+#}
 
-class exp10{
-        
-        class { 'apache':
-		docroot => '/var/www',
-	}
-	#install mpm_prefork required by php
-	class {apache:mpm_module => 'prefork'}
+#class { 'mysql::bindings':
+#	php_enable => true,
+#}
 
-	#install php
-	class {'apache::mod::php': }
+# Copy to /var/www/html
+exec { 'copy':
+	command => "cp -r /tmp/wordpress.tar.gz/wordpress/* /var/www/html/",
+	require => Class ['apache'],
+	path => ['/bin'],
+}
 
-	# Copy to /var/www/
-	exec { 'copy':
-		command => "cp -r /tmp/wordpress.tar.gz/wordpress/* /var/www/",
-		path => ['/bin'],
-	}
-
-	# Generate the wp-config.php file using the template
-	file { '/var/www/wp-config.php':
-		ensure => present,
-		require => Exec['copy'],
-		content => template("/tmp/wp-config.php.erb")
-	}
-
-	class { '::mysql::server':
-		root_password           => 'strongpassword',
-		remove_default_accounts => true
-	}
+# Generate the wp-config.php file using the template
+file { '/var/www/html/wp-config.php':
+	ensure => present,
+	require => Exec['copy'],
+	source => '/tmp/wp-config.php.erb',
 }
--- EXP-10-BusinessPortal/manual/run-10.sh
+++ EXP-10-BusinessPortal/manual/run-10.sh
@@ -1,1 +1,5 @@
 docker run -d -p 80:80 cloudopting/exp10 /usr/sbin/apache2
+
+
+
+#docker run -i -p 80:80 cloudopting/exp10 /bin/bash
