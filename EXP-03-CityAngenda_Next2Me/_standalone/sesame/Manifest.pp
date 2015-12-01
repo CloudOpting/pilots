b07904529a89f20ca31fd6c9f0053d619724cfd9
debian <debian@debian>
2015-12-01 10:53:00 +0000

    Added dockerfiles and puppet manifests for other components
    Former-commit-id: a966ff4fb3c2ac0f28bab9cce6436c3b6ece0036

--- EXP-03-CityAngenda_Next2Me/_standalone/sesame/Manifest.pp
+++ EXP-03-CityAngenda_Next2Me/_standalone/sesame/Manifest.pp
@@ -5,14 +5,17 @@ tomcat::instance { 'tomcat8':
   catalina_base => '/opt/apache-tomcat/tomcat8',
   source_url    => 'http://apache.mirror.iphh.net/tomcat/tomcat-8/v8.0.29/bin/apache-tomcat-8.0.29.tar.gz'
 }->
+tomcat::setenv::entry {'JAVA_OPTS':
+    value => "-Xms512m -Xmx2g -Dinfo.aduna.platform.appdata.basedir=/worldline/tomcat-sesame-cloud4cities/data/aduna",
+}->
 tomcat::service { 'default':
-  catalina_base => '/opt/apache-tomcat/tomcat8'
+  catalina_base => '/opt/apache-tomcat/tomcat8',
 }->
 tomcat::war { 'openrdf-sesame.war':
   catalina_base => '/opt/apache-tomcat/tomcat8',
-  war_source => '/tmp/openrdf-sesame.war'
+  war_source => 'https://github.com/CloudOpting/pilots/blob/master/EXP-03-CityAngenda_Next2Me/_standalone/sesame/openrdf-sesame.war',
 }->
 tomcat::war { 'openrdf-workbench.war':
   catalina_base => '/opt/apache-tomcat/tomcat8',
-  war_source => '/tmp/openrdf-workbench.war'
+  war_source => 'https://github.com/CloudOpting/pilots/blob/master/EXP-03-CityAngenda_Next2Me/_standalone/sesame/openrdf-workbench.war',
 }
--- EXP-03-CityAngenda_Next2Me/_standalone/tomcat/Manifest.pp
+++ EXP-03-CityAngenda_Next2Me/_standalone/tomcat/Manifest.pp
@@ -5,14 +5,17 @@ tomcat::instance { 'tomcat8':
   catalina_base => '/opt/apache-tomcat/tomcat8',
   source_url    => 'http://apache.mirror.iphh.net/tomcat/tomcat-8/v8.0.29/bin/apache-tomcat-8.0.29.tar.gz'
 }->
+tomcat::setenv::entry {'JAVA_OPTS':
+    value => "-Xms512m -Xmx2g -XX:MaxPermSize=512m -Dsolr.solr.home=/worldline/tomcat-cloud4cities/data/solr -Dsolr.backoffice.server.properties=file:/worldline/tomcat-cloud4cities/cfg/application.properties -Dmongo.backoffice.server.properties=file:/worldline/tomcat-cloud4cities/cfg/application.properties -Dsolr.api.server.properties=file:/worldline/tomcat-cloud4cities/cfg/application.properties -Dmongo.api.server.properties=file:/worldline/tomcat-cloud4cities/cfg/application.properties",
+}->
 tomcat::service { 'default':
   catalina_base => '/opt/apache-tomcat/tomcat8'
-}
+}->
 tomcat::war { 'cloud4cities-webapp-api.war':
   catalina_base => '/opt/apache-tomcat/tomcat8',
-  war_source => '/tmp/cloud4cities-webapp-api.war'
-}
+  war_source => 'https://github.com/CloudOpting/pilots/blob/master/EXP-03-CityAngenda_Next2Me/_standalone/tomcat/cloud4cities-webapp-api.war'
+}->
 tomcat::war { 'cloud4cities-webapp-solr.war':
   catalina_base => '/opt/apache-tomcat/tomcat8',
-  war_source => '/tmp/cloud4cities-webapp-solr.war'
+  war_source => 'https://github.com/CloudOpting/pilots/blob/master/EXP-03-CityAngenda_Next2Me/_standalone/tomcat/cloud4cities-webapp-solr.war'
 }
