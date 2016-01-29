722ef7bc7222d6adf13f90756452fbdb4e9012bf
gcabezas <gcabezas@gmail.com>
2016-01-29 14:25:43 +0100

    Update Manifest.pp
    Former-commit-id: 845ee6c6cc9f5df05570f24c8fea7c80e135c756

--- EXP-15-CKAN/CKAN/Manifest.pp
+++ EXP-15-CKAN/CKAN/Manifest.pp
@@ -9,37 +9,90 @@ $db_name                        ="ckan"
 $db_user                        ="ckan"
 $db_pass                        ="ckan"
 
-#INSTALL CKAN
+$hOME                           ="/root"
+$cKAN_VERSION                   ="2.2.3"
+$cKAN                           ="ckan-$cKAN_VERSION"
+$cKAN_HOME                      ="/usr/lib/ckan/default"
+$cKAN_CONFIG                    ="/etc/ckan/default"
+$cKAN_DATA                      ="/var/lib/ckan"
+
 #INSTALL PACKAGES
     exec { 'apt-get update':  path => '/usr/bin',}->
-    package { 'nginx':              ensure => 'present', }->
+    
+    package { 'python-minimal':     ensure => 'present', }->
+    package { 'python-dev':         ensure => 'present', }->
+    package { 'python-virtualenv':  ensure => 'present', }->
+    package { 'libevent-dev':       ensure => 'present', }->
+    package { 'libpq-dev':          ensure => 'present', }->
+    package { 'nginx-light':        ensure => 'present', }->
     package { 'apache2':            ensure => 'present', }->
     package { 'libapache2-mod-wsgi':ensure => 'present', }->
-    package { 'libpq5':             ensure => 'present', }->
+    package { 'postfix':            ensure => 'present', }->
+    package { 'build-essential':    ensure => 'present', }-> 
     package { 'wget':               ensure => 'present', }->
-    package { 'dpkg':               ensure => 'present', }->
+    package { 'git-core':           ensure => 'present', }-> 
+   
+# Install CKAN
 
-#DOWNLOAD AND INSTALL CKAN
-
+file { '/etc/ckan':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0750',
+  }->
+file { '/usr/lib/ckan':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0750',
+  }->
+file { "${cKAN_HOME}":
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0750',
+  }->
+file { "${cKAN_CONFIG}":
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0750',
+}->
+#Download and Install CKAN
 exec { 'download_ckan':
-  command => 'wget http://packaging.ckan.org/python-ckan_2.4-trusty_amd64.deb',
-  path    => '/usr/bin/:/bin/',
-  require => Package['wget'],
-}
+    command => 'pip install -e git+https://github.com/ckan/ckan.git@ckan-2.5.1#egg=ckan',
+    path    => '/usr/bin/:/bin/',
+    cwd     => "${cKAN_HOME}",
+    require => File["${cKAN_CONFIG}"],
+}->
+exec { 'install_requirements':
+    command => 'pip install -r /usr/lib/ckan/default/src/ckan/requirements.txt',
+    path    => '/usr/bin/:/bin/',
+    cwd     => "${cKAN_HOME}",
+    require => Exec['download_ckan'],
+}->
 
-#exec { 'install_ckan_source':
-#  command => 'dpkg -i python-ckan_2.4-trusty_amd64.deb',
-#  path    => '/usr/bin/:/bin/:/usr/bin/dpkg/',
-#  require => Exec['download_ckan'],
-#}
-package { 'install_ckan_source':
-  provider => 'dpkg',
-  source   =>"/python-ckan_2.4-trusty_amd64.deb",
-  require => Exec['download_ckan'],
+exec {'create_file':
+    command => 'paster make-config ckan /etc/ckan/default/production.ini',
+    path    => '/usr/bin/:/bin/:/usr/local/bin/',
+    cwd     => "${cKAN_CONFIG}",    
+    require => Exec['install_requirements'],
 }
 #CKAN CONFIG FILE CREATION
-  file { '/etc/ckan/default/production.ini':
+file { '/etc/ckan/default/production.ini':
   ensure  => 'file',
   content => template('/tmp/production.ini.erb'),
-  require => Package['install_ckan_source'],
+  require => Exec['create_file'],
+}
+exec { 'init_db':
+    command => 'paster db init -c /etc/ckan/default/production.ini',
+    path    => '/usr/bin/:/bin/:/usr/local/bin/',
+    cwd     => '/usr/lib/ckan/default/src/ckan',
+    require => File['/etc/ckan/default/production.ini'],
+}
+#Create Link
+exec { 'link_to_who':
+    command => 'ln -s /usr/lib/ckan/default/src/ckan/who.ini /etc/ckan/default/who.ini',
+    path    => '/usr/local/bin/:/bin/',
+    require => Exec['init_db'],
 }
